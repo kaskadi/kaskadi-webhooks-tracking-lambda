7978c2231a253483024692080bd6cee8a192c4de
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-17 18:41:04 +0100

    Added token validation

--- kaskadi-webhooks-tracking-lambda.js
+++ kaskadi-webhooks-tracking-lambda.js
@@ -1,4 +1,18 @@
+const token = process.env.YSWS_TOKEN
+
 module.exports.handler = async (event) => {
+  const eventBody = JSON.parse(event.body)
+  if (eventBody.token !== token) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        message: 'Unauthorized.'
+      })
+    }
+  }
   console.log(event.body)
   return {
     statusCode: 200,
