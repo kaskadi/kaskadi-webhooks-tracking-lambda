06620d85751eb82df01fe2f9e41226797b5cf966
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-18 11:15:01 +0100

    Added event logging to ElasticSearch

--- kaskadi-webhooks-tracking-lambda.js
+++ kaskadi-webhooks-tracking-lambda.js
@@ -1,20 +1,14 @@
+const es = require('aws-es-client')({
+  id: process.env.ES_ACCESS_ID,
+  token: process.env.ES_ACCESS_SECRET,
+  url: 'https://search-kaskadi-cl2e6mhgx3zc7ay2e5kkhjet4u.eu-central-1.es.amazonaws.com'
+})
+
 const token = process.env.YSWS_TOKEN
 
 module.exports.handler = async (event) => {
   const eventBody = JSON.parse(event.body)
-  if (eventBody.token !== token) {
-    return {
-      statusCode: 401,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        message: 'Unauthorized.'
-      })
-    }
-  }
-  console.log(event.body)
-  return {
+  const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*'
@@ -23,4 +17,28 @@ module.exports.handler = async (event) => {
       message: 'Webhook data received!'
     })
   }
+  if (eventBody.token !== token) {
+    response.statusCode = 401
+    response.body = JSON.stringify({
+      message: 'Unauthorized.'
+    })
+    return response
+  }
+  console.log(event.body)
+  esLog(eventBody)
+  return response
+}
+
+function esLog(eventBody) {
+  const timestamp = Date.now()
+  const timestampHex = timestamp.toString(16)
+  const logDocId = `${timestampHex[0]}-${timestampHex.substr(1, 5)}-${timestampHex.substr(5, 5)}`
+  es.index({
+    id: logDocId,
+    index: 'tracking-webhook-log',
+    body: {
+      date: timestamp,
+      ...eventBody
+    }
+  })
 }
