4fb7f630fc8f836166f600cee8928a394404a114
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-03 16:57:55 +0100

    Added await to fix logging

--- kaskadi-webhooks-tracking-lambda.js
+++ kaskadi-webhooks-tracking-lambda.js
@@ -25,15 +25,15 @@ module.exports.handler = async (event) => {
     return response
   }
   console.log(event.body)
-  esLog(eventBody)
+  await esLog(eventBody)
   return response
 }
 
-function esLog(eventBody) {
+async function esLog(eventBody) {
   const timestamp = Date.now()
   const timestampHex = timestamp.toString(16)
   const logDocId = `${timestampHex[0]}-${timestampHex.substr(1, 5)}-${timestampHex.substr(5, 5)}`
-  es.index({
+  await es.index({
     id: logDocId,
     index: 'ysws-logs',
     body: {
